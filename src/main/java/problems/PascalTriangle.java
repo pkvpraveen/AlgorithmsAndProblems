a640b4e428c981d445a6d5f0fa2e804dd5fc0a62
Praveenkumar V <pkvpraveen@gmail.com>
2017-05-13 13:37:45 +0530

    pascal triangle the efficient way

--- src/main/java/problems/PascalTriangle.java
+++ src/main/java/problems/PascalTriangle.java
@@ -10,14 +10,27 @@ public class PascalTriangle {
         for (int currentDepth = 0; currentDepth < depth; currentDepth++) {
             pascalTriangle[currentDepth] = new int[currentDepth + 1];
             for (int i = 0; i < currentDepth + 1; i++) {
-                if (i == 0) {
-                    pascalTriangle[currentDepth][0] = 1;
-                } else if (i == currentDepth) {
-                    pascalTriangle[currentDepth][currentDepth] = 1;
+                if (i == 0 || i == currentDepth) {
+                    pascalTriangle[currentDepth][i] = 1;
                 } else {
                     pascalTriangle[currentDepth][i] = pascalTriangle[currentDepth - 1][i - 1]
                             + pascalTriangle[currentDepth - 1][i];
                 }
+            }
+        }
+        return pascalTriangle;
+    }
+
+    public int[][] pascalTriangleEfficientWay(int depth) {
+        int[][] pascalTriangle = new int[depth][];
+        int i, number = 1, j;
+
+        for (i = 0; i < depth; i++) {
+            number = 1;
+            pascalTriangle[i] = new int[i + 1];
+            for (j = 0; j <= i; j++) {
+                pascalTriangle[i][j] = number;
+                number = number * (i - j) / (j + 1);
             }
         }
         return pascalTriangle;
--- src/test/java/problems/PascalTriangleTest.java
+++ src/test/java/problems/PascalTriangleTest.java
@@ -10,13 +10,18 @@ public class PascalTriangleTest {
     public void shouldGeneratePascalTriangle() {
         int[][] expected = {
                 {1},
-                {1,1},
+                {1, 1},
                 {1, 2, 1},
                 {1, 3, 3, 1},
                 {1, 4, 6, 4, 1}
         };
+        long startTime = System.nanoTime();
         assertArrayEquals(expected, new PascalTriangle().getPascalTriangle(5));
-
-
+        long endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
+        long startTime1 = System.nanoTime();
+        assertArrayEquals(expected, new PascalTriangle().pascalTriangleEfficientWay(5));
+        long endTime1 = System.nanoTime();
+        System.out.println(endTime1 - startTime1);
     }
 }
